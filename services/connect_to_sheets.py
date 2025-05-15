bceed3a15c247029868dfbb82b4d8d2940b7935f
Eliadc3 <82890239+Eliadc3@users.noreply.github.com>
2025-05-16 01:04:08 +0300

    Google credentials

--- services/connect_to_sheets.py
+++ services/connect_to_sheets.py
@@ -1,18 +1,25 @@
 # התחברות ל־Google Sheets בעזרת gspread + JSON
 import gspread
+import os
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 
-# הגדרת ההרשאות
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+def get_sheet():
+        
+    # הגדרת ההרשאות
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
-# טוען את הקובץ JSON שהעלית לפרויקט
-creds = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
+    # טוען את הקובץ JSON שהעלית לפרויקט
+    creds_json = os.environ["GOOGLE_CREDENTIALS"]
+    creds_dict = json.loads(creds_json)
 
-# יוצר חיבור ל־Google Sheets
-client = gspread.authorize(creds)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 
-# פותח את הגיליון לפי השם שנתת לו
-sheet = client.open("CaloriesTrackerData").sheet1
+    # יוצר חיבור ל־Google Sheets
+    client = gspread.authorize(creds)
 
-# מדפיס את הערך בתא הראשון (בדיקה)
-print(sheet.cell(1, 1).value)
+    # פותח את הגיליון לפי השם שנתת לו
+    sheet = client.open("CaloriesTrackerData").sheet1
+    print("📥 GOOGLE_CREDENTIALS loaded:", os.environ.get("GOOGLE_CREDENTIALS") is not None)
+    # מדפיס את הערך בתא הראשון (בדיקה)
+    print(sheet.cell(1, 1).value)
